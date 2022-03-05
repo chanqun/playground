ffc2e29111ce0b9031d2579bc6ba68c9d12b3d19
chanqun <cksgns93@naver.com>
2022-03-05 14:43:47 +0900

    coroutine 실습 중

--- common/src/test/kotlin/io/playground/common/Test.kt
+++ common/src/test/kotlin/io/playground/common/Test.kt
@@ -3,6 +3,7 @@ package io.playground.common
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import reactor.core.publisher.Flux
 
@@ -19,11 +20,49 @@ class Test {
     fun `coroutine scope`() {
         // 전역 스코프
         GlobalScope.launch {
+            delay(1000L) // delay는 suspend - suspend fun 은 coroutine scope 안 에서만 실행할 수 있음
+            println("World!")
+        }
+
+        println("Hello, ")
+        Thread.sleep(2000L) // thread는 blocking 하는 것
+    }
+
+    @Test
+    fun `runBlocking`() = runBlocking { // coroutine builder
+        GlobalScope.launch {
             delay(1000L)
             println("World!")
         }
 
         println("Hello, ")
-        Thread.sleep(2000L)
+        delay(2000L)
+    }
+
+    @Test
+    fun `job을 기다리자`() = runBlocking {
+        val job = GlobalScope.launch {
+            delay(2000L)
+            println("World!")
+        }
+
+        println("Hello, ")
+        job.join() // coroutine이 완료될 때 까지 기다렸다가 종료함
+    }
+
+    @Test
+    fun `join으로 job을 계속 관리해야할까`() = runBlocking {
+        // structured concurrency
+        launch {
+            delay(2000L)
+            println("World!")
+        }
+
+        launch {
+            delay(2000L)
+            println("World!")
+        }
+
+        println("Hello, ")
     }
 }
